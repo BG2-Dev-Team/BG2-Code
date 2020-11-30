8a9f51d69e3460d118fbd55e9a751329797db7c8
Ricochet <47277029+bcostello101@users.noreply.github.com>
2020-11-29 20:59:31 -0500

    Pass the victim to LobGrenade()
    Committed by Roob via GitHub

--- game/server/hl2mp/hl2mp_player.cpp
+++ game/server/hl2mp/hl2mp_player.cpp
@@ -701,8 +701,8 @@ void CHL2MP_Player::TraceAttack(const CTakeDamageInfo &info, const Vector &vecDi
 			CBaseCombatWeapon* pWeapon = pVictim->GetActiveWeapon();
 			CWeaponFrag * pGrenade = dynamic_cast<CWeaponFrag*>(pWeapon);
 			if (pGrenade && pGrenade->IsPrimed()) {
-				pGrenade->RollGrenade(pVictim);
-				pGrenade->LobGrenade(this, 100.0f); // BG3 - Roob - Stop grenade from flying away when hit in arm
+				pGrenade->LobGrenade(this, 100.0f);
+				pGrenade->LobGrenade(pVictim, 100.0f); // BG3 - Roob - Stop grenade from flying away when hit in arm
 				pGrenade->DecrementAmmo(pVictim);
 				StopSound(pGrenade->entindex(), GRENADE_FUSE_SOUND);
 				pGrenade->Remove(); //avoid grenade duplication
